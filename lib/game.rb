62a02d34d4566226372ea11b7824ea6a7eaf73ba
Lucy Fox <lucyreneefox@gmail.com>
2016-04-07 01:10:20 -0600

    get player guess working

--- lib/game.rb
+++ lib/game.rb
@@ -4,21 +4,36 @@ require './lib/messages'
 class Game
 
   def initialize
+    @column_letters = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J']
     @computer_board = Board.new(4, "Computer Board")
     @user_board = Board.new(4, "User Board")
     @computer_board.place_computer_ships
     @computer_board.output_board
+    get_player_ships
   end
 
   def get_player_ships
     message = Messages.new
     message.player_placement
     player_choice = gets.chomp.upcase.split
-    #["A1", "A2"]
+    create_coordinate_hash_array(player_choice)
   end
 
-  def create_coordinate_hash(array)
-    coordinates = {}
+  def parse_guess_for_coordinate(string)
+    coordinate_array = string.chars
+    x_location = @column_letters.find_index(coordinate_array[0])
+    y_location = (coordinate_array[1].to_i)-1 
+    {:x => x_location, :y => y_location}
+  end
+
+  def create_coordinate_hash_array(array)
+    player_coordinate_array = []
+    array.each do |location|
+      player_coordinate_array << parse_guess_for_coordinate(location)
+    end
+    puts "SDFLKJADHF"
+    puts player_coordinate_array
+    player_coordinate_array
   end
 
 
